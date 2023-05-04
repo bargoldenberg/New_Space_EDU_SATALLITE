a85437e4e266974633977b302147ac6625c8fc7a
Hagai Hen <76903853+HagaiHen@users.noreply.github.com>
2023-05-04 14:04:17 +0300

    Add files via upload
    ESP32-CAM

---
+++ ESP32-CAM/Code Examples/LedButton/LedButton.ino
@@ -0,0 +1,39 @@
+#include "esp_system.h"
+
+// Define the LED pin
+#define LED_PIN 4
+
+int lightOn = 0;
+
+const int button = 0;         //gpio to use to trigger delay
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("running setup");
+
+  pinMode(button, INPUT_PULLUP);                    //init control pin
+
+  // Initialize the LED pin as an output
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  Serial.println("running main loop");
+  int pressed = digitalRead(button);
+  
+  if (!pressed) {
+    Serial.println("button pressed");
+    if (lightOn) {
+      // Turn the LED off
+      digitalWrite(LED_PIN, LOW);
+      lightOn = 0;  
+    } else {
+      // Turn the LED on
+      digitalWrite(LED_PIN, HIGH);
+      lightOn = 1;
+    }
+    delay(500);
+  }
+
+}
