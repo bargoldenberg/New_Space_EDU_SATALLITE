fbddcf8176702755c538fc038d0699b23215f741
Hagai Hen <76903853+HagaiHen@users.noreply.github.com>
2023-05-05 13:32:10 +0300

    CameraWebServer

--- ESP32-CAM/Code Examples/CameraWebServer/app_httpd.cpp
+++ ESP32-CAM/Code Examples/CameraWebServer/app_httpd.cpp
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "esp_http_server.h"
+#include "esp_http_server.h" 
 #include "esp_timer.h"
 #include "esp_camera.h"
 #include "img_converters.h"
