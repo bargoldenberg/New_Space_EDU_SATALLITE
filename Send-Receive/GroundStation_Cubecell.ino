04182667c7493ff92aaff086569dbd4c34d43a77
Bar Goldenberg <89586016+bargoldenberg@users.noreply.github.com>
2023-06-09 16:55:54 +0300

    Create GroundStation_Cubecell.ino

---
+++ Send-Receive/GroundStation_Cubecell.ino
@@ -0,0 +1,160 @@
+/* Heltec Automation send communication test example
+ *
+ * Function:
+ * 1. Send data from a CubeCell device over hardware 
+ * 
+ * 
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/ASR650x-Arduino
+ * */
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+/*
+ * set LoraWan_RGB to 1,the RGB active in loraWan
+ * RGB red means sending;
+ * RGB green means received done;
+ */
+#ifndef LoraWan_RGB
+#define LoraWan_RGB 0
+#endif
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 255 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+float txNumber;
+bool lora_idle=true;
+bool hit = 1;
+char instruction[BUFFER_SIZE];
+String cmd;
+
+void OnTxDone( void );
+void OnTxTimeout( void );
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+
+typedef enum
+{
+    LOWPOWER,
+    RX,
+    TX
+}States_t;
+
+States_t state;
+bool sleepMode = false;
+int16_t Rssi,rxSize;
+
+void setup() {
+    Serial.begin(115200);
+
+    txNumber=0;
+    Rssi=0;
+
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    RadioEvents.RxDone = OnRxDone;
+
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+    state=TX;
+}
+
+
+
+void loop()
+{
+  switch(state)
+	{
+		case TX:
+      turnOffRGB();		
+      while(Serial.available() == 0){
+        cmd = Serial.readStringUntil('\n');
+        Serial.print("COMMAND IS: ");
+        if(cmd.length()){
+          break;
+        }
+        Serial.println(cmd);
+      }
+
+      if(lora_idle == true)
+      {
+        delay(1000);
+        txNumber += 0.01;
+        strcpy(instruction, cmd.c_str());
+        sprintf(txpacket, instruction);  //start a package
+        Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
+        turnOnRGB(COLOR_SEND,0); //change rgb color
+        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out 
+        lora_idle = false;
+        state = RX;
+      }
+		case RX:
+      delay(1000);
+			Serial.println("into RX mode");
+		    Radio.Rx(0);
+		    break;
+  }
+}
+
+
+void OnTxDone( void )
+{
+  turnOffRGB();
+  Serial.println("TX done......");
+  lora_idle = true;
+}
+
+void OnTxTimeout( void )
+{
+  turnOffRGB();
+  Radio.Sleep( );
+  Serial.println("TX Timeout......");
+  lora_idle = true;
+}
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+{
+    Rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    turnOnRGB(COLOR_RECEIVED,0);
+    Radio.Sleep( );
+
+    Serial.printf("\r\nreceived packet \"%s\" with Rssi %d , length %d\r\n",rxpacket,Rssi,rxSize);
+    Serial.println("wait to send next packet");
+
+    state=TX;
+}
